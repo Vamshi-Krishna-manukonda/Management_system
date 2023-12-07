f7880df3902ed2f653199b3e7f82bfba5a6a7eb8
Vamshi Krishna <manukondavamshi26@gmail.com>
2023-12-07 12:20:35 +0530

    Intial Commit

---
+++ src/app/employee-details/employee-sign-up/employee-sign-up.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-sign-up.component.html',
+  styleUrl: './employee-sign-up.component.scss'
+})
+export class EmployeeSignUpComponent {
+
+}
